fff89cd18998333d101d3e29ec9cd7279d4403ef
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2021-10-30 01:49:24 +0900

    GeofencingApp: Autocomplete Fragment 생성

---
+++ app/src/main/java/kr/co/bepo/geofencingapp/FirstFragment.kt
@@ -0,0 +1,61 @@
+package kr.co.bepo.geofencingapp
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.gms.common.api.Status
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
+import kr.co.bepo.geofencingapp.databinding.FragmentFirstBinding
+
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = FragmentFirstBinding.inflate(inflater, container, false)
+        .also { _binding = it }
+        .root
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Initialize the SDK
+        Places.initialize(requireContext(), getString(R.string.google_maps_key))
+
+        // Create a new PlacesClient instance
+        val placesClient = Places.createClient(requireContext())
+
+        // Initialize the AutocompleteSupportFragment.
+        val autocompleteFragment =
+            childFragmentManager.findFragmentById(R.id.autocomplete_fragment)
+                    as AutocompleteSupportFragment
+
+        // Specify the types of place data to return.
+        autocompleteFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME))
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onPlaceSelected(place: Place) {
+                Log.i("TAG", "Place: ${place.name}, ${place.id}")
+            }
+
+            override fun onError(status: Status) {
+                Log.i("TAG", "An error occurred: $status")
+            }
+        })
+    }
+}
