bbde23de29e40b299a93001d58df72191f8ba1b4
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2021-11-06 21:43:01 +0900

    GeofencingApp: Google Cloud Platform 연결

--- app/src/main/java/kr/co/bepo/geofencingapp/MainActivity.kt
+++ app/src/main/java/kr/co/bepo/geofencingapp/MainActivity.kt
@@ -2,12 +2,32 @@ package kr.co.bepo.geofencingapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kr.co.bepo.geofencingapp.databinding.ActivityMainBinding
+import kr.co.bepo.geofencingapp.util.Permissions
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        initNavigtaion()
+    }
+
+    private fun initNavigtaion() = with(binding) {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        if (Permissions.hasLocationPermission(this@MainActivity)) {
+            val action = PermissionFragmentDirections.actionPermissionFragmentToMapsFragment()
+            navController.navigate(action)
+        }
     }
 }
