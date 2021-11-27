46761b206f33e0f889f9977ee2a281f1f0d84cbc
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2021-11-27 20:41:10 +0900

    GeofencingApp: GeofencesAdapter 생성

---
+++ app/src/main/java/kr/co/bepo/geofencingapp/adapters/GeofencesAdapter.kt
@@ -0,0 +1,39 @@
+package kr.co.bepo.geofencingapp.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import kr.co.bepo.geofencingapp.data.GeofenceEntity
+import kr.co.bepo.geofencingapp.databinding.GeofencesRowLayoutBinding
+import kr.co.bepo.geofencingapp.util.MyDiffUtil
+
+class GeofencesAdapter: RecyclerView.Adapter<GeofencesAdapter.MyViewHolder>() {
+
+    private var geofencesEntity = mutableListOf<GeofenceEntity>()
+
+    inner class MyViewHolder(private val binding: GeofencesRowLayoutBinding): RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(geofenceEntity: GeofenceEntity) = with(binding) {
+
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder =
+        MyViewHolder(GeofencesRowLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bind(geofencesEntity[position])
+    }
+
+    override fun getItemCount(): Int =
+        geofencesEntity.size
+
+    fun setData(newGeofencesEntity: MutableList<GeofenceEntity>) {
+        val geofenceDiffUtil = MyDiffUtil(geofencesEntity, newGeofencesEntity)
+        val diffUtilResult = DiffUtil.calculateDiff(geofenceDiffUtil)
+        geofencesEntity = newGeofencesEntity
+        diffUtilResult.dispatchUpdatesTo(this)
+    }
+}
--- app/src/main/java/kr/co/bepo/geofencingapp/ui/geofences/GeofencesFragment.kt
+++ app/src/main/java/kr/co/bepo/geofencingapp/ui/geofences/GeofencesFragment.kt
@@ -5,15 +5,49 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import kr.co.bepo.geofencingapp.R
+import kr.co.bepo.geofencingapp.adapters.GeofencesAdapter
+import kr.co.bepo.geofencingapp.databinding.FragmentGeofencesBinding
+import kr.co.bepo.geofencingapp.viewmodels.SharedViewModel
 
 class GeofencesFragment : Fragment() {
+
+    private var _binding: FragmentGeofencesBinding? = null
+    private val binding get() = _binding!!
+
+    private val sharedViewModel: SharedViewModel by activityViewModels()
+    private val geofencesAdapter by lazy { GeofencesAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_geofences, container, false)
+    ): View = FragmentGeofencesBinding.inflate(inflater, container, false)
+        .also { _binding = it }
+        .root
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initViews()
+    }
+
+    private fun initViews() = with(binding) {
+        geofencesRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        geofencesRecyclerView.adapter = geofencesAdapter
+
+        observeDatabase()
+    }
+
+    private fun observeDatabase() {
+        sharedViewModel.readGeofences.observe(viewLifecycleOwner) {
+            geofencesAdapter.setData(it)
+        }
     }
 }
