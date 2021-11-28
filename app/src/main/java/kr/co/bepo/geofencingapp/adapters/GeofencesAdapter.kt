fd2c9fe706ee9f9753a64cb31fc1f1b5bdb6dfa4
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2021-11-28 21:04:27 +0900

    GeofencingApp: Geofences RecyclerView Data Bind

--- app/src/main/java/kr/co/bepo/geofencingapp/adapters/GeofencesAdapter.kt
+++ app/src/main/java/kr/co/bepo/geofencingapp/adapters/GeofencesAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
 import kr.co.bepo.geofencingapp.data.GeofenceEntity
 import kr.co.bepo.geofencingapp.databinding.GeofencesRowLayoutBinding
 import kr.co.bepo.geofencingapp.util.MyDiffUtil
@@ -15,7 +16,16 @@ class GeofencesAdapter: RecyclerView.Adapter<GeofencesAdapter.MyViewHolder>() {
     inner class MyViewHolder(private val binding: GeofencesRowLayoutBinding): RecyclerView.ViewHolder(binding.root) {
 
         fun bind(geofenceEntity: GeofenceEntity) = with(binding) {
+            snapshotImageView.load(geofenceEntity.snapshot)
+            nameTextView.text = geofenceEntity.name
+            locationValueTextView.text = geofenceEntity.location
+            latitudeTextView.text = parseCoordinates(geofenceEntity.latitude)
+            longitudeTextView.text = parseCoordinates(geofenceEntity.longitude)
+            radiusValueTextView.text = geofenceEntity.radius.toString()
+        }
 
+        private fun parseCoordinates(value: Double): String {
+            return  String.format("%.4f", value)
         }
 
     }
